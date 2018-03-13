0d3afef145980723b7d76d871234bc05907f8626
Faulo <FauloLio@gmail.com>
2018-03-13 20:01:32 +0100

    Slothsoft\Farah\Module\Node\AssetBuilderTrait

---
+++ src/Slothsoft/Farah/Module/Node/AssetBuilderTrait.php
@@ -0,0 +1,36 @@
+<?php
+namespace Slothsoft\Farah\Module\Node;
+
+use Slothsoft\Core\XML\LeanElement;
+use Slothsoft\Farah\Module\Module;
+use Slothsoft\Farah\Module\Node\Asset\FragmentAsset;
+
+trait AssetBuilderTrait {
+    protected function buildFragment(string $name, array $useDocumentList, string $useTemplate) : FragmentAsset {
+        $children = [];
+        foreach ($useDocumentList as $key => $useDocument) {
+            $attributes = [];
+            $attributes['ref'] = $useDocument;
+            if (is_string($key)) {
+                $attributes['as'] = $key;
+            }
+            $children[] = LeanElement::createOneFromArray(
+                Module::TAG_USE_DOCUMENT,
+                $attributes
+            );
+        }
+        $children[] = LeanElement::createOneFromArray(
+            Module::TAG_USE_TEMPLATE,
+            ['ref' => $useTemplate]
+        );
+        
+        $element = LeanElement::createOneFromArray(
+            Module::TAG_FRAGMENT,
+            ['name' => $name],
+            $children
+        );
+        
+        return $this->createChildNode($element);
+    }
+}
+
