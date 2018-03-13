4955b3f8b5d4c60d02d61301c79a2f0d12ddd4e2
Faulo <FauloLio@gmail.com>
2018-03-13 20:04:25 +0100

    buildExternalDocument

--- src/Slothsoft/Farah/Module/Node/AssetBuilderTrait.php
+++ src/Slothsoft/Farah/Module/Node/AssetBuilderTrait.php
@@ -3,9 +3,20 @@ namespace Slothsoft\Farah\Module\Node;
 
 use Slothsoft\Core\XML\LeanElement;
 use Slothsoft\Farah\Module\Module;
+use Slothsoft\Farah\Module\Node\Asset\ExternalDocumentAsset;
 use Slothsoft\Farah\Module\Node\Asset\FragmentAsset;
 
 trait AssetBuilderTrait {
+    protected function buildExternalDocument(string $name, string $href) : ExternalDocumentAsset {
+        $element = LeanElement::createOneFromArray(
+            Module::TAG_EXTERNAL_DOCUMENT,
+            [
+                'name' => $name,
+                'href' => $href,
+            ]
+        );
+        return $this->createChildNode($element);
+    }
     protected function buildFragment(string $name, array $useDocumentList, string $useTemplate) : FragmentAsset {
         $children = [];
         foreach ($useDocumentList as $key => $useDocument) {
