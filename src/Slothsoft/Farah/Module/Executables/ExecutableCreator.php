2434d72afe15c9264251d9be13ab25cb9601d3bd
Faulo <FauloLio@gmail.com>
2018-04-27 13:11:37 +0200

    Executables

--- src/Slothsoft/Farah/Module/Executables/ExecutableCreator.php
+++ src/Slothsoft/Farah/Module/Executables/ExecutableCreator.php
@@ -11,47 +11,40 @@ use Psr\Http\Message\MessageInterface;
 
 class ExecutableCreator
 {
-    protected $ownerAsset;
-    protected $type;
+    private $ownerAsset;
+    private $type;
     public function __construct(AssetInterface $ownerAsset, FarahUrlArguments $args) {
         $this->ownerAsset = $ownerAsset;
         $this->args = $args;
     }
     
-    public function createNullExecutable() : ExecutableInterface {
-        $executable = new NullExecutable();
+    protected function initExecutable(ExecutableInterface $executable) : ExecutableInterface {
         $executable->init($this->ownerAsset, $this->args);
         return $executable;
+    }
+    
+    public function createNullExecutable() : ExecutableInterface {
+        return $this->initExecutable(new NullExecutable());
     }
     
     public function createMessageExecutable(MessageInterface $message) : ExecutableInterface {
-        $executable = new NullExecutable();
-        $executable->init($this->ownerAsset, $this->args);
-        return $executable;
+        return $this->initExecutable(new NullExecutable());
     }
     
     public function createTransformationExecutable(string $name, InstructionCollector $collector) : ExecutableInterface {
-        $executable = new TransformationExecutable($name, $collector);
-        $executable->init($this->ownerAsset, $this->args);
-        return $executable;
+        return $this->initExecutable(new TransformationExecutable($name, $collector));
     }
     
     public function createXmlFile(string $pathToFile) : ExecutableInterface {
-        $executable = new XmlFile($pathToFile);
-        $executable->init($this->ownerAsset, $this->args);
-        return $executable;
+        return $this->initExecutable(new XmlFile($pathToFile));
     }
     
     public function createHtmlFile(string $pathToFile) : ExecutableInterface {
-        $executable = new HtmlFile($pathToFile);
-        $executable->init($this->ownerAsset, $this->args);
-        return $executable;
+        return $this->initExecutable(new HtmlFile($pathToFile));
     }
     
     public function createBinaryFile(string $pathToFile) : ExecutableInterface {
-        $executable = new BinaryFile($pathToFile);
-        $executable->init($this->ownerAsset, $this->args);
-        return $executable;
+        return $this->initExecutable(new BinaryFile($pathToFile));
     }
     
     
