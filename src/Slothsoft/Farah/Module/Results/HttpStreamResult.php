f1ef50f565e80b860dd8953cf996c13b6685bdc4
Faulo <FauloLio@gmail.com>
2018-05-02 11:45:45 +0200

    PumpStream

--- src/Slothsoft/Farah/Module/Results/HttpStreamResult.php
+++ src/Slothsoft/Farah/Module/Results/HttpStreamResult.php
@@ -8,6 +8,7 @@ use Slothsoft\Core\Calendar\Seconds;
 use Slothsoft\Core\IO\HTTPStream;
 use Slothsoft\Farah\Exception\StreamTimedOutException;
 use Exception;
+use Generator;
 
 /**
  *
@@ -23,66 +24,7 @@ class HttpStreamResult extends ResultBase
     
     public function lookupStream() : StreamInterface
     {
-        return new PumpStream(
-            function() {
-                $intervalTime = 0;
-                $timeoutTime = 0;
-                $sleepDuration = $this->stream->getSleepDuration();
-                $heartbeatContent = $this->stream->getHeartbeatContent();
-                $heartbeatInterval = $this->stream->getHeartbeatInterval();
-                $heartbeatTimeout = $this->stream->getHeartbeatTimeout();
-                $heartbeatEOL = $this->stream->getHeartbeatEOL();
-                $heartbeatSent = false;
-                while (! connection_aborted()) {
-                    try {
-                        $status = $this->stream->getStatus();
-                        if ($timeoutTime > $heartbeatTimeout) {
-                            throw new StreamTimedOutException(get_class($this->stream));
-                        }
-                    } catch (Exception $e) {
-                        $status = HTTPStream::STATUS_ERROR;
-                    }
-                    switch ($status) {
-                        case HTTPStream::STATUS_CONTENTDONE:
-                            $content = $this->stream->getContent();
-                            if ($content !== '') {
-                                if ($heartbeatSent and $heartbeatEOL !== null) {
-                                    yield $heartbeatEOL;
-                                    $heartbeatSent = false;
-                                }
-                                yield $content;
-                            }
-                            // fallthrouuugh /o/
-                        case HTTPStream::STATUS_DONE:
-                        case HTTPStream::STATUS_ERROR:
-                            break 2;
-                        case HTTPStream::STATUS_CONTENT:
-                            $content = $this->stream->getContent();
-                            if ($content !== '') {
-                                if ($heartbeatSent and $heartbeatEOL !== null) {
-                                    yield $heartbeatEOL;
-                                    $heartbeatSent = false;
-                                }
-                                $intervalTime = 0;
-                                $timeoutTime = 0;
-                                yield $content;
-                            }
-                            break;
-                        case HTTPStream::STATUS_RETRY:
-                            $intervalTime += $sleepDuration;
-                            $timeoutTime += $sleepDuration;
-                            usleep($sleepDuration * Seconds::USLEEP_FACTOR);
-                            if ($intervalTime > $heartbeatInterval and $heartbeatContent !== null) {
-                                $intervalTime = 0;
-                                yield $heartbeatContent;
-                                $heartbeatSent = true;
-                            }
-                            break;
-                    }
-                }
-                return false;
-            }
-        );
+        return new PumpStream($this->yieldContent());
     }
     
     public function lookupMimeType() : string
@@ -106,6 +48,66 @@ class HttpStreamResult extends ResultBase
     
     public function lookupHash() : string {
         return '';
+    }
+    
+    private function yieldContent() : Generator {
+        yield '';
+        $intervalTime = 0;
+        $timeoutTime = 0;
+        $sleepDuration = $this->stream->getSleepDuration();
+        $heartbeatContent = $this->stream->getHeartbeatContent();
+        $heartbeatInterval = $this->stream->getHeartbeatInterval();
+        $heartbeatTimeout = $this->stream->getHeartbeatTimeout();
+        $heartbeatEOL = $this->stream->getHeartbeatEOL();
+        $heartbeatSent = false;
+        while (! connection_aborted()) {
+            try {
+                $status = $this->stream->getStatus();
+                if ($timeoutTime > $heartbeatTimeout) {
+                    throw new StreamTimedOutException(get_class($this->stream));
+                }
+            } catch (Exception $e) {
+                $status = HTTPStream::STATUS_ERROR;
+            }
+            switch ($status) {
+                case HTTPStream::STATUS_CONTENTDONE:
+                    $content = $this->stream->getContent();
+                    if ($content !== '') {
+                        if ($heartbeatSent and $heartbeatEOL !== null) {
+                            yield $heartbeatEOL;
+                            $heartbeatSent = false;
+                        }
+                        yield $content;
+                    }
+                    // fallthrouuugh /o/
+                case HTTPStream::STATUS_DONE:
+                case HTTPStream::STATUS_ERROR:
+                    break 2;
+                case HTTPStream::STATUS_CONTENT:
+                    $content = $this->stream->getContent();
+                    if ($content !== '') {
+                        if ($heartbeatSent and $heartbeatEOL !== null) {
+                            yield $heartbeatEOL;
+                            $heartbeatSent = false;
+                        }
+                        $intervalTime = 0;
+                        $timeoutTime = 0;
+                        yield $content;
+                    }
+                    break;
+                case HTTPStream::STATUS_RETRY:
+                    $intervalTime += $sleepDuration;
+                    $timeoutTime += $sleepDuration;
+                    usleep($sleepDuration * Seconds::USLEEP_FACTOR);
+                    if ($intervalTime > $heartbeatInterval and $heartbeatContent !== null) {
+                        $intervalTime = 0;
+                        yield $heartbeatContent;
+                        $heartbeatSent = true;
+                    }
+                    break;
+            }
+        }
+        return false;
     }
 }
 
