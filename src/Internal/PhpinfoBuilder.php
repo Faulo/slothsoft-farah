3b05800f4bf8d12b50d2404274d8b24f554336be
Daniel Schulz <faulolio@gmail.com>
2022-01-04 23:03:07 +0100

    Properly close output buffer

--- src/Internal/PhpinfoBuilder.php
+++ src/Internal/PhpinfoBuilder.php
@@ -2,18 +2,18 @@
 declare(strict_types = 1);
 namespace Slothsoft\Farah\Internal;
 
+use Slothsoft\Core\IO\Writable\Adapter\FileWriterFromStringWriter;
 use Slothsoft\Core\IO\Writable\Delegates\StringWriterFromStringDelegate;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\FileWriterResultBuilder;
-use Slothsoft\Core\IO\Writable\Adapter\FileWriterFromStringWriter;
 
 /**
  *
  * @author Daniel Schulz
- *        
+ *
  */
 class PhpinfoBuilder implements ExecutableBuilderStrategyInterface {
 
@@ -22,7 +22,7 @@ class PhpinfoBuilder implements ExecutableBuilderStrategyInterface {
             ob_start();
             phpinfo();
             $data = ob_get_contents();
-            ob_clean();
+            ob_end_clean();
             if (PHP_SAPI === 'cli') {
                 $data = '<pre>' . htmlentities($data, ENT_XML1 | ENT_DISALLOWED, 'UTF-8') . '</pre>';
             }
