f1e8a692f5e1ad52d3a59504d818b245a0731099
Slothsoft CI (Madeline (Unity)) <info.slothsoft@gmail.com>
2025-10-17 13:33:23 +0200

    test: test_fragment_doesNotChangeResult

--- tests/Module/Executable/ExecutableTest.php
+++ tests/Module/Executable/ExecutableTest.php
@@ -3,17 +3,42 @@ declare(strict_types = 1);
 namespace Slothsoft\Farah\Module\Executable;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Constraint\IsIdentical;
+use Slothsoft\Farah\FarahUrl\FarahUrl;
+use Slothsoft\Farah\Module\Module;
 
 /**
  * ExecutableTest
  *
  * @see Executable
- *
- * @todo auto-generated
  */
 class ExecutableTest extends TestCase {
     
     public function testClassExists(): void {
         $this->assertTrue(class_exists(Executable::class), "Failed to load class 'Slothsoft\Farah\Module\Executable\Executable'!");
+    }
+    
+    /**
+     *
+     * @dataProvider sameResultProvider
+     */
+    public function test_fragment_doesNotChangeResult(string $url, string $fragment): void {
+        $url = FarahUrl::createFromReference($url, Module::getBaseUrl());
+        
+        $left = Module::resolveToResult($url);
+        $right = Module::resolveToResult($url->withFragment($fragment));
+        
+        $this->assertThat($left, new IsIdentical($right));
+    }
+    
+    public function sameResultProvider(): iterable {
+        yield 'phpinfo default' => [
+            '/phpinfo',
+            ''
+        ];
+        yield 'phpinfo #xml' => [
+            '/phpinfo',
+            'xml'
+        ];
     }
 }
