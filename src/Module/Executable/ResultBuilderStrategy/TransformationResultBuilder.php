adc675cde5d2eebeef3d154daf82e5a967fb5895
Slothsoft CI (Madeline (Unity)) <info.slothsoft@gmail.com>
2025-10-18 14:17:27 +0200

    fix: restore cache

--- src/Module/Executable/ResultBuilderStrategy/TransformationResultBuilder.php
+++ src/Module/Executable/ResultBuilderStrategy/TransformationResultBuilder.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 namespace Slothsoft\Farah\Module\Executable\ResultBuilderStrategy;
 
+use Slothsoft\Core\IO\Writable\Decorators\DOMWriterMemoryCache;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\FarahUrl\FarahUrlStreamIdentifier;
 use Slothsoft\Farah\LinkDecorator\DecoratedDOMWriter;
@@ -78,6 +79,8 @@ class TransformationResultBuilder implements ResultBuilderStrategyInterface {
                 }
             }
         }
+        
+        $writer = new DOMWriterMemoryCache($writer);
         
         $streamBuilder = new DOMWriterStreamBuilder($writer, 'transformation');
         return new ResultStrategies($streamBuilder);
