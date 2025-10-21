b5a4f6c490ca6b806bfcbf2617fd0386b4b265ec
Daniel Schulz (Madeline) <faulolio@gmail.com>
2025-10-21 10:47:04 +0200

    feat: more schema support

--- src/Schema/SchemaLocator.php
+++ src/Schema/SchemaLocator.php
@@ -16,6 +16,9 @@ class SchemaLocator {
     
     public function findSchemaLocation(DOMDocument $document): ?string {
         if ($node = $document->documentElement) {
+            if ($node->hasAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'noNamespaceSchemaLocation')) { //DOMHelper::NS_XSI
+                return $node->getAttributeNS('http://www.w3.org/2001/XMLSchema-instance', 'noNamespaceSchemaLocation');
+            }
             if ($ns = $node->namespaceURI) {
                 if (strpos($ns, self::NAMESPACE_BASE) === 0) {
                     $version = $node->hasAttribute(self::ATTR_VERSION) ? $node->getAttribute(self::ATTR_VERSION) : self::DEFAULT_VERSION;
@@ -25,6 +28,11 @@ class SchemaLocator {
                     }
                     $url = "farah://slothsoft@$schema[0]/schema/$schema[1]/$version";
                     return $url;
+                }
+            } else {
+                switch ($node->localName) {
+                    case 'testsuites':
+                        return 'farah://slothsoft@schema/schema/junit/1.0';
                 }
             }
         }
--- tests/Schema/SchemaLocatorTest.php
+++ tests/Schema/SchemaLocatorTest.php
@@ -3,17 +3,69 @@ declare(strict_types = 1);
 namespace Slothsoft\Farah\Schema;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Constraint\IsEqual;
+use DOMDocument;
 
 /**
  * SchemaLocatorTest
  *
  * @see SchemaLocator
- *
- * @todo auto-generated
  */
 class SchemaLocatorTest extends TestCase {
     
     public function testClassExists(): void {
         $this->assertTrue(class_exists(SchemaLocator::class), "Failed to load class 'Slothsoft\Farah\Schema\SchemaLocator'!");
+    }
+    
+    /**
+     *
+     * @dataProvider provideXmlWithSchema
+     */
+    public function test_findSchemaLocation(string $xml, ?string $expected): void {
+        $document = new DOMDocument();
+        $document->loadXML($xml);
+        
+        $sut = new SchemaLocator();
+        
+        $actual = $sut->findSchemaLocation($document);
+        
+        self::assertThat($actual, new IsEqual($expected));
+    }
+    
+    public static function provideXmlWithSchema(): iterable {
+        yield 'nothing' => [
+            '<data/>',
+            null
+        ];
+        
+        yield 'default version' => [
+            '<data xmlns="http://schema.slothsoft.net/farah/sitemap"/>',
+            'farah://slothsoft@farah/schema/sitemap/1.0'
+        ];
+        
+        yield 'explicit version' => [
+            '<data xmlns="http://schema.slothsoft.net/farah/sitemap" version="0.1"/>',
+            'farah://slothsoft@farah/schema/sitemap/0.1'
+        ];
+        
+        yield 'unknown schema' => [
+            '<data xmlns="http://www.w3.org/1999/xhtml" version="5.0"/>',
+            null
+        ];
+        
+        yield 'schemaLocation' => [
+            '<phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://schema.phpunit.de/9.6/phpunit.xsd"/>',
+            'https://schema.phpunit.de/9.6/phpunit.xsd'
+        ];
+        
+        yield 'schemaLocation and slothsoft namespace' => [
+            '<phpunit xmlns="http://schema.slothsoft.net/farah/sitemap" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://schema.phpunit.de/8.5/phpunit.xsd"/>',
+            'https://schema.phpunit.de/8.5/phpunit.xsd'
+        ];
+        
+        yield 'junit special schema' => [
+            '<testsuites/>',
+            'farah://slothsoft@schema/schema/junit/1.0'
+        ];
     }
 }
