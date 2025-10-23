8c9c5bcb804685d471844d6efc66a9803c35ed1b
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-10-23 17:34:32 +0200

    refactor: FarahServerTestCase

--- tests/API/XSL/HtmlTest.php
+++ tests/API/XSL/HtmlTest.php
@@ -2,43 +2,21 @@
 declare(strict_types = 1);
 namespace Slothsoft\Farah\API\XSL;
 
-use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Constraint\IsEqual;
-use Slothsoft\FarahTesting\FarahServer;
-use Slothsoft\FarahTesting\Exception\BrowserDriverNotFoundException;
+use Slothsoft\FarahTesting\FarahServerTestCase;
 use Slothsoft\Farah\FarahUrl\FarahUrlAuthority;
-use Symfony\Component\Panther\Client;
 
-class HtmlTest extends TestCase {
+final class HtmlTest extends FarahServerTestCase {
     
-    private FarahServer $server;
-    
-    private ?Client $client = null;
-    
-    protected function setUp(): void {
-        $this->server = new FarahServer();
-        $this->server->setModule(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'test-module'), realpath('test-files/test-module'));
-        $this->server->start();
-        
-        try {
-            $this->client = $this->server->createClient();
-        } catch (BrowserDriverNotFoundException $e) {
-            $this->markTestSkipped();
-        }
+    protected static function setUpServer(): void {
+        self::$server->setModule(FarahUrlAuthority::createFromVendorAndModule('slothsoft', 'test-module'), 'test-files/test-module');
     }
     
-    protected function tearDown(): void {
-        if ($this->client) {
-            $this->client->quit();
-            unset($this->client);
-        }
-        
-        unset($this->server);
+    protected function setUpClient() {
+        $this->client->request('GET', '/slothsoft@test-module/tests/html');
     }
     
     public function test_titleIsUrl(): void {
-        $this->client->request('GET', '/slothsoft@test-module/tests/html');
-        
         $actual = $this->client->executeScript(<<<EOT
 return document.querySelector("title").textContent;
 EOT);
