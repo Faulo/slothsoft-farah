a71c4d6961ab52e1390a58d92b8bda0819711c38
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-12-31 20:06:25 +0100

    refactor(PhpinfoBuilderTest): move test

--- tests/Internal/PhpinfoBuilderTest.php
+++ tests/Internal/PhpinfoBuilderTest.php
@@ -3,17 +3,52 @@ declare(strict_types = 1);
 namespace Slothsoft\Farah\Internal;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Constraint\IsEqual;
 
 /**
  * PhpinfoBuilderTest
  *
  * @see PhpinfoBuilder
- *
- * @todo auto-generated
  */
 final class PhpinfoBuilderTest extends TestCase {
     
     public function testClassExists(): void {
         $this->assertTrue(class_exists(PhpinfoBuilder::class), "Failed to load class 'Slothsoft\Farah\Internal\PhpinfoBuilder'!");
+    }
+    
+    private const REFERENCE = 'farah://slothsoft@farah/phpinfo';
+    
+    private string $phpinfo;
+    
+    public function setUp(): void {
+        ob_start();
+        phpinfo();
+        $data = ob_get_contents();
+        ob_end_clean();
+        
+        $this->phpinfo = '<pre>' . htmlentities($data, ENT_XML1 | ENT_DISALLOWED, 'UTF-8') . '</pre>';
+    }
+    
+    /**
+     *
+     * @dataProvider countProvider
+     */
+    public function test_read(int $count): void {
+        for ($i = 0; $i < $count; $i ++) {
+            $actual = file_get_contents(self::REFERENCE);
+            $this->assertThat($actual, new IsEqual($this->phpinfo));
+        }
+    }
+    
+    public function countProvider(): iterable {
+        yield 'once' => [
+            1
+        ];
+        yield 'twice' => [
+            2
+        ];
+        yield 'thrice' => [
+            3
+        ];
     }
 }
