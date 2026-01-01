fe581bbd904502ec5bd78141efe2366129dd3a52
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-01 17:29:40 +0100

    test(request): query

--- tests/API/Domain/ParamTest.php
+++ tests/API/Domain/ParamTest.php
@@ -23,6 +23,14 @@ final class ParamTest extends TestCase {
     private static function requestPage(string $path): string {
         $_SERVER['REQUEST_URI'] = $path;
         
+        if ($query = parse_url($path, PHP_URL_QUERY)) {
+            $args = [];
+            parse_str($query, $args);
+            foreach ($args as $key => $value) {
+                $_GET[$key] = $value;
+            }
+        }
+        
         $request = MessageFactory::createServerRequest($_SERVER, $_REQUEST, $_FILES);
         
         Kernel::setCurrentRequest($request);
@@ -56,6 +64,9 @@ final class ParamTest extends TestCase {
     <file name="request-with-param" ref="/current-request" status-active="" status-public="" title="request-with-param" uri="/request-with-param" url="http://test.slothsoft.net/request-with-param">
         <sfm:param name="param" value="value"/>
     </file>
+    <file name="request-with-query" ref="/current-request?param=value" status-active="" status-public="" title="request-with-query" uri="/request-with-query" url="http://test.slothsoft.net/request-with-query">
+        <sfm:param name="param" value="should not see this"/>
+    </file>
 </domain>
 EOT
         ];
@@ -67,10 +78,37 @@ EOT
 EOT
         ];
         
+        yield '/request?param=value' => [
+            '/request?param=value',
+            <<<EOT
+<request-info xmlns="http://schema.slothsoft.net/farah/module" url="http://localhost/request?param=value" ref="farah://slothsoft@farah/current-request?param=value">
+    <param name="param" value="value"/>
+</request-info>
+EOT
+        ];
+        
         yield '/request-with-param' => [
             '/request-with-param',
             <<<EOT
 <request-info xmlns="http://schema.slothsoft.net/farah/module" url="http://localhost/request-with-param" ref="farah://slothsoft@farah/current-request?param=value">
+    <param name="param" value="value"/>
+</request-info>
+EOT
+        ];
+        
+        yield '/request-with-param?param=override' => [
+            '/request-with-param?param=override',
+            <<<EOT
+<request-info xmlns="http://schema.slothsoft.net/farah/module" url="http://localhost/request-with-param?param=override" ref="farah://slothsoft@farah/current-request?param=override">
+    <param name="param" value="override"/>
+</request-info>
+EOT
+        ];
+        
+        yield '/request-with-query' => [
+            '/request-with-query',
+            <<<EOT
+<request-info xmlns="http://schema.slothsoft.net/farah/module" url="http://localhost/request-with-query" ref="farah://slothsoft@farah/current-request?param=value">
     <param name="param" value="value"/>
 </request-info>
 EOT
