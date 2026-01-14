f47eb99a320d696f09762a4222a03b251129d3bf
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-14 22:21:22 +0100

    test(SitemapJsonBuilder)

---
+++ tests/Internal/SitemapJsonBuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Slothsoft\Farah\Internal;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * SitemapJsonBuilderTest
+ *
+ * @see SitemapJsonBuilder
+ *
+ * @todo auto-generated
+ */
+final class SitemapJsonBuilderTest extends TestCase {
+    
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(SitemapJsonBuilder::class), "Failed to load class 'Slothsoft\Farah\Internal\SitemapJsonBuilder'!");
+    }
+}
